bcddba0066cae0df53f9ed02540669c96391c1ca
Ryan Williams <ryan@runsascoded.com>
2024-11-09 17:12:13 -0500

    v0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dvc-utils',
-    version="0.0.7",
+    version="0.0.8",
     description="CLI for diffing DVC files at two commits (or one commit vs. current worktree), optionally passing both through another command first",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
